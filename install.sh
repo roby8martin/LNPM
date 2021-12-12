f2cf20d111b4ccd1c3621728d85b5b880c0b14ae
roby8martin <roby8martin@gmail.com>
2021-12-12 10:01:47 +0100

    aggiunto yes sugli update e upgrade

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
-echo 'Stai per nstallare i seguenti servizi:'
+echo 'Stai per installare i seguenti servizi:'
 echo ''
 echo 'Nginx, Php, MySQL e PhpMyAdmin'
-echo -n 'Continuare? (Y/n)'
+echo 'Continuare? (Y/n):'
 
 read RISPOSTA
 
-if [${RISPOSTA} = "y"]
+if [ ${RISPOSTA} = "y" ]
 then
   LIST_WEB_SERVICE="nginx php7.3-fpm php7.3-mysql php7.3-mbstring mariadb-server"
 
   # Aggiorna i repositori
-  apt update  
-  apt upgrade
+  apt update -y
+  apt upgrade -y
 
   apt install -y $LIST_OF_APPS
 
