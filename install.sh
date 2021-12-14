2862cd6ef2c555940867c60d2f2660c947d78e2d
roby8martin <roby8martin@gmail.com>
2021-12-14 06:35:29 +0100

    Modificato nome file di nginx e configurazione dell'installer

--- install.sh
+++ install.sh
@@ -21,24 +21,27 @@ then
   apt install mariadb-server -y
 
   # Modifica i file di configurazione di Nginx
-  printf '\nCreo un backup per il file di configurazione di Nginx'
+  printf '\nCreo un backup per il file di configurazione di Nginx\n'
   cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default_backup
 
-  printf '\nCopio file di confiugrazione per Nginx'
-  cp conf/nginx/default /etc/nginx/sites-enabled/default
+  printf '\nCopio file di confiugrazione per Nginx\n'
+  cp -r conf/nginx/default.conf /etc/nginx/sites-enabled/default
 
   # Modifica i file di configurazione di Php
-  printf '\nCreo un backup per il file di configurazione di Php'
+  printf '\nCreo un backup per il file di configurazione di Php\n'
   cp /etc/php/7.4/fpm/php.ini /etc/php/7.4/fpm/php_backup.ini
 
-  printf '\nCopio file di confiugrazione per Php'
-  cp conf/php7.4/php.ini /etc/php/7.4/fpm/php.ini
+  printf '\nCopio file di confiugrazione per Php\n'
+  cp -r conf/php7.4/php.ini /etc/php/7.4/fpm/php.ini
+
+  printf '\nRiavvio Web Server Nginx\n'
+  /etc/init.d/nginx reload
 
   # Installo PhpMyAdmin
-  printf '\nSto installando PhpMyAdmin'
+  printf '\nSto installando PhpMyAdmin\n'
   cp -r phpmyadmin/ /var/www/html/phpmyadmin
 
-  printf '\nCreo cartella tmp e do i permessi di lettura e scrittura per PhpMyAdmin'
+  printf '\nCreo cartella tmp e do i permessi di lettura e scrittura per PhpMyAdmin\n'
   mkdir /var/www/html/phpmyadmin/tmp
   chmod 077 /var/www/html/phpmyadmin/tmp
 
