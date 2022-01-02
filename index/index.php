b0a1c7540059a9f9df9764981450baa7f6f7b36c
roby8martin <43995710+roby8martin@users.noreply.github.com>
2022-01-02 16:23:15 +0100

    Update index.php

--- index/index.php
+++ index/index.php
@@ -1,6 +1,6 @@
 <?php
-  //$ip=exec("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'");
-  $ip='192.168.1.118';
+  $ip=exec("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'");
+  //$ip='192.168.1.118';
 ?>
 <!doctype html>
 <html lang="it">
